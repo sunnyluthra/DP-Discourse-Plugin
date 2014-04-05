6b9d98e5a36fc4f59ae2dce044b14290557fee96
Sunny Luthra <sunny@mrova.com>
2014-04-05 14:01:00 +0530

    first commit

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: Discourse Discuss Politics
+# about: DP's Custom functionalities
+# version: 0.2
+# author: mrova.com
+# minimum Discourse version: v0.9.9
+register_asset("javascripts/discourse/templates/header.js.handlebars")
